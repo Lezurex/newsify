7034237484a2e86efe55633957d3188a35edadc1
Lenny <lenny@familie-angst.ch>
2022-06-21 08:59:30 +0200

    🎉 Initialize Spring Boot

---
+++ src/main/java/com/lezurex/newsify/NewsifyApplication.java
@@ -0,0 +1,13 @@
+package com.lezurex.newsify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsifyApplication.class, args);
+	}
+
+}
