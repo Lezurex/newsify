48d3158324c174abc0182f1d56f60378c2c7b880
Lenny <lenny@familie-angst.ch>
2022-06-21 09:34:19 +0200

    🏗️ Add base entities

---
+++ src/main/java/com/lezurex/newsify/entities/Category.java
@@ -0,0 +1,24 @@
+package com.lezurex.newsify.entities;
+
+import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Entity
+@Data
+public class Category {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String name;
+
+  @OneToMany
+  private List<Article> articles;
+
+}
