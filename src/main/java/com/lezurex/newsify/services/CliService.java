b59e857b406b49cf221a1f48e99e03ff50d40b99
Maurus Graf <maurus.graf@ergon.ch>
2022-07-05 09:42:23 +0200

    🎨 improve code structure
    Signed-off-by: Maurus Graf <maurus.graf@ergon.ch>

--- src/main/java/com/lezurex/newsify/services/CliService.java
+++ src/main/java/com/lezurex/newsify/services/CliService.java
@@ -29,48 +29,65 @@ public class CliService implements CommandLineRunner {
 	@Override
 	public void run(String... args) {
 		System.out.println("Welcome to Newsify!");
-		fetcherService.fetchAll();
 		List<Category> categories = categoryService.getCategories();
 		if (categories.size() <= 0) {
 			System.out.println("Sorry, there are no categories! There's nothing to view.");
 		} else {
 			while (true) {
-				System.out.println("Which Category do you want to view? Enter the corresponding number.");
-				for (int i = 0; i < categories.size(); i++) {
-					System.out.println("[" + (i + 1) + "]: " + categories.get(i).getName());
-				}
-				System.out.println("[0]: Exit");
+				System.out.println("Fetching articles...");
 				fetcherService.fetchAll();
-				String number = scanner.nextLine();
-				int chosenCategoryIndex;
-				try {
-					chosenCategoryIndex = Integer.parseInt(number) - 1;
-				}catch (NumberFormatException numberFormatException){
-					System.out.println("Please do not enter letters!");
-					chosenCategoryIndex = -2;
-				}
-				if (chosenCategoryIndex == -1) {
-					System.exit(SpringApplication.exit(context));
-				}
-				if (categories.size() - 1 >= chosenCategoryIndex && chosenCategoryIndex >= -1) {
+				displayCategories(categories);
+				int chosenCategoryIndex = getChosenCategoryIndexFromUser();
+				if (isCategoryIndexValid(categories, chosenCategoryIndex)) {
+					if (chosenCategoryIndex == -1) {
+						System.exit(SpringApplication.exit(context));
+					}
 					Category chosenCategory = categories.get(chosenCategoryIndex);
 					List<Article> articles = categoryService.getRecentArticlesOfCategory(chosenCategory);
-					if (articles.size() <= 0) {
-						System.out.println("Sorry, there are no articles in this category!");
-					} else {
-						for (Article article : articles) {
-							System.out.println(article.getTitle());
-							System.out.println(article.getDescription());
-							System.out.println(article.getLink());
-							System.out.println(article.getPubDate());
-							System.out.println("–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––");
-						}
-						System.out.println("\n");
-					}
+					displayArticles(articles);
 				}else {
 					System.out.println("The category you chose does not exist! Choose another category");
 				}
 			}
 		}
+	}
+
+	private boolean isCategoryIndexValid(List<Category> categories, int chosenCategoryIndex) {
+		return categories.size() - 1 >= chosenCategoryIndex && chosenCategoryIndex >= -1;
+	}
+
+	private void displayArticles(List<Article> articles) {
+		if (articles.size() <= 0) {
+			System.out.println("Sorry, there are no articles in this category!");
+		} else {
+			for (Article article : articles) {
+				System.out.println(article.getTitle());
+				System.out.println(article.getDescription());
+				System.out.println(article.getLink());
+				System.out.println(article.getPubDate());
+				System.out.println("–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––");
+			}
+			System.out.println("\n");
+		}
+	}
+
+	private int getChosenCategoryIndexFromUser() {
+		String number = scanner.nextLine();
+		int chosenCategoryIndex;
+		try {
+			chosenCategoryIndex = Integer.parseInt(number) - 1;
+		}catch (NumberFormatException numberFormatException){
+			System.out.println("Please do not enter letters!");
+			chosenCategoryIndex = -2;
+		}
+		return chosenCategoryIndex;
+	}
+
+	private void displayCategories(List<Category> categories) {
+		System.out.println("Which Category do you want to view? Enter the corresponding number.");
+		for (int i = 0; i < categories.size(); i++) {
+			System.out.println("[" + (i + 1) + "]: " + categories.get(i).getName());
+		}
+		System.out.println("[0]: Exit");
 	}
 }
