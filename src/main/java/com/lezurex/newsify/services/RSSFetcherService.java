499807dd70b746cac66477a097d043753f2ecb0b
Lenny <lenny@familie-angst.ch>
2022-07-05 08:42:37 +0200

    ✨ Add RSSFetcher

--- src/main/java/com/lezurex/newsify/services/RSSFetcherService.java
+++ src/main/java/com/lezurex/newsify/services/RSSFetcherService.java
@@ -1,16 +1,27 @@
 package com.lezurex.newsify.services;
 
+import com.lezurex.newsify.database.IArticleRepo;
 import com.lezurex.newsify.database.IRSSFeedRepo;
+import com.lezurex.newsify.entities.Article;
 import com.lezurex.newsify.entities.RSSFeed;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.FeedException;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
 import org.springframework.stereotype.Service;
-
+import java.io.IOException;
+import java.net.URL;
 import java.util.List;
+
 @Service
 public class RSSFetcherService implements IRSSFetcherService {
-	IRSSFeedRepo feedRepo;
 
-	public RSSFetcherService(IRSSFeedRepo feedRepo) {
+	private final IRSSFeedRepo feedRepo;
+	private final IArticleRepo articleRepo;
+
+	public RSSFetcherService(IRSSFeedRepo feedRepo, IArticleRepo articleRepo) {
 		this.feedRepo = feedRepo;
+		this.articleRepo = articleRepo;
 	}
 
 	@Override
@@ -21,6 +32,22 @@ public class RSSFetcherService implements IRSSFetcherService {
 
 	@Override
 	public void fetchFeed(RSSFeed rssFeed) {
-
+		SyndFeedInput input = new SyndFeedInput();
+		try {
+			SyndFeed feed = input.build(new XmlReader(new URL(rssFeed.getUrl())));
+			var entries = feed.getEntries();
+			var iterator = entries.iterator();
+			while (iterator.hasNext()) {
+				var entry = iterator.next();
+				if (!articleRepo.existsById(entry.getUri())) {
+					Article article =
+							new Article(entry.getUri(), entry.getTitle(), entry.getDescription().getValue(),
+									entry.getPublishedDate(), entry.getLink(), rssFeed.getCategory(), rssFeed);
+					articleRepo.save(article);
+				}
+			}
+		} catch (IllegalArgumentException | FeedException | IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
